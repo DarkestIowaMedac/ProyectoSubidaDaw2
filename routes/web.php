20e6bcb339a0a2746531adc76bf2318482fdcad5
JesusGomez99 <jgl0033@alu.medac.es>
2025-02-14 17:54:18 +0100

    formulario listo

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/formulario', function () {
+    return Inertia::render('Formulario');
+})->middleware(['auth', 'verified'])->name('formulario');
+
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

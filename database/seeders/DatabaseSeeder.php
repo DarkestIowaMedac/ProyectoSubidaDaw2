ff53049f0bd7c4c187a6b29b384a8ca235343d31
PabloChDAW <pcg0031@alu.medac.es>
2025-02-15 18:57:14 +0100

    Añadir muestras simplificadas, sedes y sus factorías y seeders
    - Incluyo el diagrama e-r.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Muestra;
+use App\Models\Sede;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Especifica los datos reales de las 15 sedes
+        $sedes = [
+            ['codigo' => 'A', 'nombre' => 'Albacete'],
+            ['codigo' => 'AL', 'nombre' => 'Alicante'],
+            ['codigo' => 'ALII', 'nombre' => 'Alicante II'],
+            ['codigo' => 'I', 'nombre' => 'Almería'],
+            ['codigo' => 'C', 'nombre' => 'Córdoba'],
+            ['codigo' => 'L', 'nombre' => 'Leganés'],
+            ['codigo' => 'G', 'nombre' => 'Granada'],
+            ['codigo' => 'H', 'nombre' => 'Huelva'],
+            ['codigo' => 'J', 'nombre' => 'Jerez'],
+            ['codigo' => 'M', 'nombre' => 'Madrid'],
+            ['codigo' => 'MG', 'nombre' => 'Málaga'],
+            ['codigo' => 'MU', 'nombre' => 'Murcia'],
+            ['codigo' => 'S', 'nombre' => 'Sevilla'],
+            ['codigo' => 'V', 'nombre' => 'Valencia'],
+            ['codigo' => 'Z', 'nombre' => 'Zaragoza']
+        ];
 
+        // Crea las sedes
+        foreach ($sedes as $sede) {
+            Sede::create($sede);
+        }
+
+        // Crea un usuario de prueba
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'user',
+            'email' => 'user@user.com',
         ]);
+
+        // Crea 15 muestras de prueba
+        Muestra::factory()->count(10)->create();
     }
 }

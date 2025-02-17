a44ac716994bafa503cb7c8b45ca868057232d6d
javi19xx <fsc0005@alu.medac.es>
2025-02-17 19:35:01 +0100

    UsuarioController
    Un controlador para sacar la id del usuario logueado

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\MuestraController;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -35,5 +36,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::middleware('auth')->get('/user/id', [UsuarioController::class, 'idUsuario']);
+
 
 require __DIR__.'/auth.php';
